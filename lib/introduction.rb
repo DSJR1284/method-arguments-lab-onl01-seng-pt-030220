f385f8b525efab71021870d56f8019cce1234ea3
beloved-scraper-9319 <dsjr1284@gmail.com>
2020-03-05 01:56:35 +0000

    Automatically backed up by Learn

---
+++ lib/introduction.rb
@@ -0,0 +1,8 @@
+def introduction(name)
+  puts "Hi, my name is #{name}."
+  end
+puts introduction
+
+def introduction_with_language (name, language)
+  put "Hi, my name is #{name} and I am learning to program in #{language}."
+  
